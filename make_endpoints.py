7e95fe317ff3cfbec2bc1d37635a30227448c2f8
H Guy <eehgu@login2.arc4.leeds.ac.uk>
2020-07-05 20:41:20 +0100

    ..

--- make_endpoints.py
+++ make_endpoints.py
@@ -28,14 +28,15 @@ widths = mean_widths + cluster_widths * nclusters
 
 # Get event filesbase_dir='/Users/heather/Desktop/Everest/everest-flexpart/'
 #out_dir='/Users/heather/Desktop/Everest/everest-flexpart/endpoints/'
-base_dir='/nobackup/eehgu/everest-flexpart/events/'
-out_dir='/nobackup/eehgu/everest-flexpart/events/endpoints-525/'
+base_dir='/nobackup/eehgu/everest-flexpart/events_masl/'
+out_dir='/nobackup/eehgu/everest-flexpart/events_masl/endpoints/'
 
-release_times = list(set([x[-37:-23] for x in glob.glob(base_dir +'20*')]))
+#release_times = list(set([x[-37:-23] for x in glob.glob(base_dir +'20*')]))
+release_times = list(set([x[-38:-24] for x in glob.glob(base_dir +'20*')]))
 #end_times = list(set([x[-22:-8] for x in glob.glob(base_dir +'20*')]))
 end_times = [dt.datetime.strftime((dt.datetime.strptime(rt,'%Y%m%d%H%M%S') - dt.timedelta(hours=72)),'%Y%m%d%H%M%S') for rt in release_times]
 
-pressure=525
+pressure=3730
 
 for i in range(0,len(release_times)):
     rt = release_times[i]
