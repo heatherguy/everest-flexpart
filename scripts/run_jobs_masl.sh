dc8b651a0f0282fecef2f6b9833ab617f7674310
Heather Guy <heatherguy@users.noreply.github.com>
2020-07-05 13:30:46 +0100

    run jobs masl

---
+++ scripts/run_jobs_masl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ALL_DIRS=$(\ls -1d /nobackup/eehgu/everest-flexpart/events_masl/*)
+
+for DIR in ${ALL_DIRS}
+do
+  pushd ${DIR}
+  if [ ! -e output/grid*.nc ] ; then
+    qsub -l h_rt=6:00:00 run_job.sh
+  fi
+  popd
+done
